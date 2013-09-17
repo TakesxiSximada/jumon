0149392326fb8a1d4d87c85a722ab2fab82a6814
TakesxiSximada <takesxi.sximada@gmail.com>
2013-09-17 10:24:46 +0900

    exampleのサブコマンドでmain()内で複数の処理に分岐するケースを追加

--- examples/testcmd/command/bbb/ddd/__init__.py
+++ examples/testcmd/command/bbb/ddd/__init__.py
@@ -1,6 +1,35 @@
 #-*- coding: utf-8 -*-
+import jumon
+
+def a_func(argv):
+    print 'a_func', argv
+
+def b_func(argv):
+    print 'b_func', argv
+
+def c_func(argv):
+    print 'c_func', argv
+
+CMD = {'a': a_func,
+       'b': b_func,
+       'c': c_func,
+       }
 
 def main(argv):
-    print 'ddd'
+    parser = jumon.TransparentOptionParser()
+    parser.add_option('--common-option', default=False, action='store_true')
+    opts, args = parser.parse_args(argv)
 
+    try:
+        cmd = args[0]
+    except IndexError as err:
+        parser.error(err)
+
+    sub_args = args[1:]
+
+    try:
+        func = CMD[cmd]
+    except KeyError as err:
+        parser.error(err)
+    return func(sub_args)
 
