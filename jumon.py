034d754548b71625d85056dd8a2ba259b41f8ee4
TakesxiSximada <takesxi.sximada@gmail.com>
2014-03-06 11:25:07 +0900

    version を 1.2に変更

--- jumon.py
+++ jumon.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 """The small framework for sub commands.
 """
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 __all__ = ['TransparentOptionParser',
            'TransparentArgumentParser',
            'Shell',
