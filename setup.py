6056ca44676d6a0539dcbd80258b73cca3aede77
TakesxiSximada <takesxi.sximada@gmail.com>
2014-03-24 21:48:58 +0900

    typo

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ classifiers = [
 
 setup(
     name='jumon',
-    version='1.1.1'
+    version='1.1.1',
     url='http://sximadaw3.web.fc2.com/work/jumon',
     download_url='https://bitbucket.org/takesxi_sximada/jumon',
     license='Apache License 2.0',
